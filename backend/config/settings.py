adcb233e62f98c4c1ad8b5b20e8a907ea94b8591
rogerioalves7 <rogerio.alves3@fatec.sp.gov.br>
2025-12-26 15:03:01 -0300

    Ajuste no settings.py para teste do Timeout

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -186,16 +186,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:5173')
 
 
-# --- CONFIGURAÇÃO DE EMAIL (GMAIL) ---
-
+# --- CONFIGURAÇÃO DE EMAIL (BREVO/GMAIL/RESEND) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-# O cast=int é importante para garantir que a porta seja número
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-# O cast=bool (ou lógica similar) é importante aqui
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
-
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+# --- CORREÇÃO DO BOOLEANO ---
+# Converte a string 'True' do env file para o booleano True do Python
+# Se não estiver definido, assume os padrões seguros para porta 587
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
